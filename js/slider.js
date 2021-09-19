94b7b3dec8f04b95912ca1c0c32179b346396185
Nataly <80852688+Nataly20Nataly@users.noreply.github.com>
2021-09-20 00:48:10 +0300

    Created header and not finish banner part

---
+++ js/slider.js
@@ -0,0 +1,7 @@
+var myCarousel = document.querySelector("#carouselExampleCaptions")
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 1000,
+  wrap: true,
+  keyboard: true,
+  touch: true,
+})
